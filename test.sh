25d57ad2372b4db4572dd975d4bf14df6754af55
Connor2803 <87635341+Connor2803@users.noreply.github.com>
2023-05-08 15:10:49 +0800

    Clean Type_of_Breach and Remove Columns
    Removed all data after the slash or comma in the Date_of_Breach field using awk sub and a regular expression.
    
    Then removed the Location_of_Breached_Information and Summary fields using the complement of cut.

--- test.sh
+++ test.sh
@@ -1,1 +1,32 @@
-echo "wow"
+#!/bin/bash
+
+# check if input file exists
+if [[ ! -f $2 ]]; then
+    echo "Error: File does not exist or is zero-length"
+    exit 1
+fi
+
+case "$1" in
+    preprocess)
+        #preprocess code
+        echo "preprocess time"
+        # cuts off non-needed fields
+        #sed -i 's/\([^,\/]*\).*/\1/' "$2"
+        cleantype=$(awk 'BEGIN{FS=OFS="\t"} {sub(/[,\/].*/, "", $5)} 1' $2 > "temp.tsv")
+
+        cutoff=$(cut --complement -f6,7 "temp.tsv" | awk '{print}')
+        echo $cutoff
+        ;;
+    breaches_per_month)
+        echo "breach time"
+        ;;
+    *)
+        echo "Usage: cyber_breaches preprocess or breaches_per_month"
+        exit 1
+        ;;
+
+esac
+
+input=$2
+
+#echo $(cat $input)
